f7e9ae593cdcf2a1527ffc938dafe5077ae8060f
KrisHHFI <kristopherpepperuk@gmail.com>
2024-08-24 22:57:21 +0300

    Background scrolling effect

--- src/app/PageOne.tsx
+++ src/app/PageOne.tsx
@@ -1,23 +1,33 @@
 "use client";
 
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import Image from "next/image";
 
 const PageOne: React.FC = () => {
-
+    const [scrollY, setScrollY] = useState(0);
     const assetPrefix = process.env.NODE_ENV === 'production' ? '/NextJS-Tailwind-CSS-Site' : '';
+
+    useEffect(() => {
+        const handleScroll = () => {
+            setScrollY(window.scrollY);
+        };
+
+        window.addEventListener('scroll', handleScroll);
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
 
     return (
         <main>
-            <div className="PageContainer">
-                <Image
-                    src={`${assetPrefix}/Treehouse.png`}
-                    alt="Treehouse image"
-                    className="TreehouseImage"
-                    width={500}
-                    height={500}
-                />
-            </div>
+            <Image
+                src={`${assetPrefix}/Treehouse.png`}
+                alt="Treehouse image"
+                className="TreehouseImage"
+                style={{ top: `${-scrollY * 0.2}px` }} // Use a negative multiplier to move the image up
+                width={500}
+                height={500}
+            />
         </main>
     );
 };
