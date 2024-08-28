47ad89cd6b7c74837452a35d68ec1de63f11057b
KrisHHFI <kristopherpepperuk@gmail.com>
2024-08-28 14:25:11 +0300

    GitHub pages stuff

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const isProd = process.env.NODE_ENV === 'production';
+
+const nextConfig = {
+  output: 'export',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/NextJS-Tailwind-CSS-Site/' : '',
+  images: {
+    unoptimized: true, // Since GitHub Pages doesn't support image optimization
+  },
+};
 
 export default nextConfig;
